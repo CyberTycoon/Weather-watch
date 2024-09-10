b745919d972c98b1ed12b654e596f962de62afa4
silas okanlawon <silasokanla2006@gmail.com>
2024-09-10 12:23:57 +0100

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    // any other Next.js config options
+  };
+  
+  export default nextConfig;
+  
